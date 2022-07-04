6d7f2ba015a63da9db612b5f3a891bfb9b2bf35d
Maridinah <ahono.evelyn@gmail.com>
2022-07-04 05:59:52 -0700

    commit for the 4-puts.c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
-int main(void);
+/**
+ * main - Entry point
+ *
+ * Description: 'Write a C program that prints exactly using the Put function'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle);
-return(0)
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
