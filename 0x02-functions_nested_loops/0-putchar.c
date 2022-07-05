afeb30b88b216de75458ca680a198841d3effe19
Maridinah <ahono.evelyn@gmail.com>
2022-07-05 13:49:10 -0700

    correction on the main 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include<main.h>
+#include "main.h"
 /**
  * main - Entry point
  *
  * Description: 'program that prints _putchar'
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
