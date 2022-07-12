14080c1471c19a914c2d832e0a183848e7dbef0c
Maridinah <ahono.evelyn@gmail.com>
2022-07-12 14:08:43 -0700

    a commit on the 0x05. C - Pointers, arrays and strings tasks

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _atoi - Write a function that convert a string to an integer
+ *
+ * @s: input
+ *
+ * Return: Always 0 (Success)
+ */
+int _atoi(char *s)
+{
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * 101-crackme - a program that generates random valid passwords
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+return (0);
+}
