470c5b9a4258910495a89f51679d76a4b8e2ff91
Maridinah <ahono.evelyn@gmail.com>
2022-07-13 13:56:14 -0700

    a commit on the 3-strcmp.c task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: Param1
+ * @s2: Param2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+int result;
+result = strcmp(s1, s2);
+return (result);
+}
