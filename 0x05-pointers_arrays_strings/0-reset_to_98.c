d63cbc386f4b298cfaacbf3578f0b39d077c5fcd
Maridinah <ahono.evelyn@gmail.com>
2022-07-14 16:21:38 -0700

    a commit on all the main.h file correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,6 +5,8 @@
  * @n: pointer to n to be updated
  */
 void reset_to_98(int *n)
+
 {
 *n = 98;
+
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,13 +5,11 @@
  * @a: input 1
  *
  * @b: input 2
- *
- * Return: integers
  */
 void swap_int(int *a, int *b)
 {
-int n;
-n = *a;
+int tmp;
+tmp = *a;
 *a = *b;
-*b = n;
+*b = tmp;
 }
