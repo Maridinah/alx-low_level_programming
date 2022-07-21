3aa6d5bfa312628ba1b7fb7f58c12ce378ecc3b5
Maridinah <ahono.evelyn@gmail.com>
2022-07-21 10:15:13 -0700

    hot fix on spaces

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
  *
@@ -6,23 +7,18 @@
  */
 
 int main(void)
-
 {
-
 int i;
 
 printf("Infinite loop incoming :(\n");
 i = 0;
+
 /**
  * main - while (i < 10)
- *
  * {
- *
- *   putchar(i);
- *
+ * putchar(i);
  * }
  */
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
